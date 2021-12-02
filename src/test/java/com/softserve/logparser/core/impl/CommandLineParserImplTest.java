4d8ec8b84953fd1fb28e923aace61b4be4a0b8a7
Oleh <arkhiolog@gmail.com>
2021-12-02 23:18:20 +0200

    Update CommandLineParserImplTest

--- src/test/java/com/softserve/logparser/core/impl/CommandLineParserImplTest.java
+++ src/test/java/com/softserve/logparser/core/impl/CommandLineParserImplTest.java
@@ -4,6 +4,7 @@ import com.softserve.logparser.core.CommandLineParser;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
@@ -11,7 +12,7 @@ import java.util.stream.Stream;
 import static org.junit.jupiter.api.Assertions.*;
 
 
-public class CommandLineParserImplTest {
+class CommandLineParserImplTest {
 
     @DisplayName("False")
     @Test
@@ -21,13 +22,12 @@ public class CommandLineParserImplTest {
         assertFalse(cliParser.parse(zero));
     }
 
-
-    static Stream<String[]> stringArrayProvider() {
+    static Stream<Arguments> stringArrayProvider() {
         return Stream.of(
-                new String[]{"apache_logs.txt"},
-                new String[]{"-sc=2xx -d apache_logs.txt"},
-                new String[]{"-ip -l=10 -a apache_logs_1.txt apache_logs_2.txt"},
-                new String[]{"-size -l=30 -from=2016-02-14T18:00:00 apache_logs_1.txt"}
+                Arguments.of((Object) new String[]{"apache_logs.txt"}),
+                Arguments.of((Object) new String[]{"-sc=2xx -d apache_logs.txt"}),
+                Arguments.of((Object) new String[]{"-ip -l=10 -a apache_logs_1.txt apache_logs_2.txt"}),
+                Arguments.of((Object) new String[]{"-size -l=30 -from=2016-02-14T18:00:00 apache_logs_1.txt"})
 
         );
     }
